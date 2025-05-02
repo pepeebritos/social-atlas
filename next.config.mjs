780f4ee7434b81507f7a1c8679f271dd5adf5703
Pedro Britos <pedrobritos@Pedros-MacBook-Pro.local>
2025-05-02 14:31:09 -0700

    🚀 Added clean @username route rewrite to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/@:username',
+        destination: '/username-profile/:username',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
